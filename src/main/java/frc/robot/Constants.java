7e73203466240a79b78fb55ffc3e9552a20975da
Fireblade2534 <Fireblade5234@gmail.com>
2026-06-04 11:16:25 -0400

    commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    public static final double FORWARD_SPEED = 0.4;
+    public static final double FORWARD_SPEED = 0.2;
     public static final double TURN_SPEED = 0.3;
     public static final double CONTROLLER_DEADBAND = 0.05;
   }
